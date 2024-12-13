e7566d922fb2776e6504fc2a4d3751383b4c60d2
JamesVictor-o <victorjames40v@gmail.com>
2024-12-13 13:31:09 +0100

    create why we stand out section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import Features from "@/components/sections/Features";
 import Hero from "@/components/sections/Hero";
 import HowItWorks from "@/components/sections/HowItWorks";
 import Pricing from "@/components/sections/Pricing";
+import StandOutSection from "@/components/standout/Standout";
 
 export default function Home() {
 	return (
@@ -12,6 +13,7 @@ export default function Home() {
 			<Features />
 			<HowItWorks />
 			<Benefits />
+			<StandOutSection/>
 			<Pricing />
 			<CTA />
 		</>
