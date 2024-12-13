e7b32c857050d545ddf664b899aa03d693d1ce4f
diegoTech14 <diegotech1499@gmail.com>
2024-12-13 15:21:12 -0600

    feat: Added why choose paystell section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,11 +4,13 @@ import Features from "@/components/sections/Features";
 import Hero from "@/components/sections/Hero";
 import HowItWorks from "@/components/sections/HowItWorks";
 import Pricing from "@/components/sections/Pricing";
+import ChooseUs from "@/components/layout/ChooseUs";
 
 export default function Home() {
 	return (
 		<>
 			<Hero />
+			<ChooseUs />
 			<Features />
 			<HowItWorks />
 			<Benefits />
---
+++ src/components/layout/ChooseUs.tsx
@@ -0,0 +1,46 @@
+const ChooseUs = () => {
+    return (
+        <div>
+<div className="bg-gray-50 py-24 sm:py-32">
+  <div className="mx-auto max-w-2xl px-6 lg:max-w-7xl lg:px-8">
+    <h2 className="text-center text-base/7 font-semibold text-indigo-600">BENEFITS </h2>
+    <p className="mx-auto mt-2 max-w-lg text-balance text-center text-4xl font-semibold tracking-tight text-gray-950 sm:text-5xl">Why Choose Paystell?</p>
+    <p className="mt-8 text-pretty text-center text-lg font-medium text-gray-500 sm:text-xl/8">Say Start collecting payments effortlessly with PayStell. Simplify your process, skip the hassle, and focus on what matters most—growing your business</p>
+    <div className="mt-10 grid gap-4 sm:mt-16 lg:grid-cols-3 lg:grid-rows-2">
+      <div className="relative lg:row-span-2">
+        <div className="absolute inset-px rounded-lg bg-white lg:rounded-l-[2rem]">
+
+        </div>
+        
+        <div className="pointer-events-none absolute inset-px rounded-lg shadow ring-1 ring-black/5 lg:rounded-l-[2rem]"></div>
+      </div>
+      <div className="relative max-lg:row-start-1">
+        <div className="absolute inset-px rounded-lg bg-white max-lg:rounded-t-[2rem]"></div>
+
+        <div className="pointer-events-none absolute inset-px rounded-lg shadow ring-1 ring-black/5 max-lg:rounded-t-[2rem]"></div>
+      </div>
+      <div className="relative max-lg:row-start-3 lg:col-start-2 lg:row-start-2">
+        <div className="absolute inset-px rounded-lg bg-white"></div>
+
+        <div className="pointer-events-none absolute inset-px rounded-lg shadow ring-1 ring-black/5"></div>
+      </div>
+      <div className="relative lg:row-span-2">
+        <div className="absolute inset-px rounded-lg bg-white max-lg:rounded-b-[2rem] lg:rounded-r-[2rem]"></div>
+        <div className="relative flex h-full flex-col overflow-hidden rounded-[calc(theme(borderRadius.lg)+1px)] max-lg:rounded-b-[calc(2rem+1px)] lg:rounded-r-[calc(2rem+1px)]">
+
+          <div className="relative min-h-[30rem] w-full grow">
+
+          </div>
+        </div>
+        <div className="pointer-events-none absolute inset-px rounded-lg shadow ring-1 ring-black/5 max-lg:rounded-b-[2rem] lg:rounded-r-[2rem]"></div>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+        </div>
+    )
+}
+
+export default ChooseUs;
