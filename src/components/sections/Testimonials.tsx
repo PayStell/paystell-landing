f07b83526c7fe6e9d51907c5802e5ba321ed8d7b
villarley <115122095+Villarley@users.noreply.github.com>
2024-12-14 16:11:37 -0600

    Feat: added basic structure and Star.svg

---
+++ src/components/sections/Testimonials.tsx
@@ -0,0 +1,16 @@
+import { Button } from "@/components/ui/button";
+
+const Testimonials = () => {
+	return (
+		<section className="h-800 relative pt-32 pb-24 lg:pt-40 lg:pb-32 bg-background">
+			<div className="container">
+				<div className="max-w-3xl mx-auto text-center bg-primary">
+
+
+				</div>
+			</div>
+		</section>
+	);
+};
+
+export default Testimonials;
