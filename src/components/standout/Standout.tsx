e757106720562ec786b3dd23cc3047ad40a75576
Man <mparedes.digital@gmail.com>
2024-12-18 00:53:30 -0300

    fix: minimal fixes

--- src/components/standout/Standout.tsx
+++ src/components/standout/Standout.tsx
@@ -1,20 +1,19 @@
-
 import Image from "next/image"
 import ImageOne from "@/components/standout/assets/star.svg"
 import ImageTwo from  "@/components/standout/assets/Frame 2147223741.png"
 import ImageThree from  "@/components/standout/assets/tick-circle.svg"
 import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
+
 const StandOutSection = () => {
   return (
-		<section id="how-it-works" className="py-24 bg-background">
-			<div className="container flex flex-col md:flex-row md:justify-between md:items-start">
-     
-      <div className="md:w-[608px]">
-          <div className="flex mb-4">
-            <Badge className="w-fit bg-primary/10 px-3 rounded-xl hover:bg-primary/10">
-              <h2 className="text-lg font-bold text-primary mr-3">FEATURES</h2>
-              <Image src="/star.svg" width={28} height={28} alt="star" />
+		<section id="standout" className="py-24 bg-background">
+			<div className="container mx-auto flex flex-col md:flex-row gap-8 items-start">
+				<div className="flex flex-col flex-1">
+					<div className="flex mb-4 animate-pulse">
+						<Badge className="w-fit bg-primary/10 px-3 rounded-xl hover:bg-primary/10">
+							<h2 className="text-lg font-bold text-primary mr-3">FEATURES</h2>
+							<Image src="/star.svg" width={28} height={28} alt="star" />
 						</Badge>
 					</div>	
         <h2 className="text-2xl md:text-3xl font-semibold mt-4 mb-2">
@@ -25,10 +24,7 @@ const StandOutSection = () => {
           process, skip the hassle, and focus on what matters most—growing your
           business.
         </p>
-      </div>
-  
-      
-      <div className="mt-14 md:w-96">
+        <div className="mt-14">
         <ul className="space-y-3">
           <li className="flex items-center text-[#333333] text-lg md:text-xl leading-6 md:leading-7">
             <div className="w-6 h-6 mr-3 animate-bounce">
@@ -80,27 +76,25 @@ const StandOutSection = () => {
           </li>
         </ul>
       </div>
-  
-     
-          <div className="mt-10 flex flex-col sm:flex-row gap-4">
-            <Button size="lg">
-							Get Started Now
-						</Button>
-          </div>
+      <div className="mt-10 flex flex-col sm:flex-row gap-4">
+        <Button size="lg">
+          Get Started Now
+        </Button>
+      </div>
+      </div>
+      <div className="flex-1 flex justify-end">
+        <Image
+          src={ImageTwo}
+          width={500}
+          height={300}
+          className="w-full h-72 object-contain"
+          alt="Why We Stand Out Image"
+        />
+      </div>
     </div>
-  
-  
-	<div className="w-full md:w-1/2 flex justify-center items-center animate-fade-in mt-10 md:mt-0">
-	  <Image
-		  src={ImageTwo}
-		  width={500}
-		  height={300}
-		  className="w-full h-72 object-contain"
-		  alt="Why We Stand Out Image"
-	  />
-	</div>
   </section>
   )
 }
 
 export default StandOutSection
+
